03fdc1d8ba9eee2c576507b817d486bc11fbd79d
ambyyy <boystrn364@gmail.com>
2022-09-08 14:34:48 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,37 +1,12 @@
-/*
- *
- *  * File: 4-puts.c
- *
- *   * Auth: Brennan D Baraban <375@holbertonschool.com>
- *
- *    */
-
-
-
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - Prints "Programming is like building a multilingual
- *
- *   *                puzzle, followed by a new line.
- *
- *    *
- *
- *     * Return: Always 0.
- *
- *      */
-
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-
-
-			return (0);
-
-}
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}}
